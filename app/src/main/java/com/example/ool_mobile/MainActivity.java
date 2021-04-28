0601615da08b80ac06a9792de24d8c65a25eeaaf
Júlia Braz <63269350+zulia21@users.noreply.github.com>
2021-04-28 14:10:20 -0300

    -Test variable for commit

--- app/src/main/java/com/example/ool_mobile/MainActivity.java
+++ app/src/main/java/com/example/ool_mobile/MainActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int test;
-    String test1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
