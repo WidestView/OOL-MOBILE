ada568a57e500bafe08eeca89cebfa113f250f14
Júlia Braz <63269350+zulia21@users.noreply.github.com>
2021-07-22 18:26:17 -0300

    Login InputLayout problem solved

--- app/src/main/java/com/example/ool_mobile/MainActivity.java
+++ app/src/main/java/com/example/ool_mobile/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
