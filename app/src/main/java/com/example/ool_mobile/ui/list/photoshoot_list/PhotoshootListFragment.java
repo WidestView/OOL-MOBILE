028e34475488a7563c7acf9c5ac73be3fc3d18af
figurantpp <renan.r.marcelino2@protonmail.com>
2021-08-29 11:13:13 -0300

    Added PhotoshootListFragment

---
+++ app/src/main/java/com/example/ool_mobile/ui/list/photoshoot_list/PhotoshootListFragment.java
@@ -0,0 +1,12 @@
+package com.example.ool_mobile.ui.list.photoshoot_list;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.ool_mobile.R;
+
+public class PhotoshootListFragment extends Fragment {
+
+    public PhotoshootListFragment() {
+        super(R.layout.fragment_photoshoot_list);
+    }
+}
