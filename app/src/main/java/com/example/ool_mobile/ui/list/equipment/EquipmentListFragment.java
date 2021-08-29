52f99a2bb64695b4fc432fe45bdad87abd628683
figurantpp <renan.r.marcelino2@protonmail.com>
2021-08-29 12:13:52 -0300

    Added AddEquipmentActivity

--- app/src/main/java/com/example/ool_mobile/ui/list/equipment/EquipmentListFragment.java
+++ app/src/main/java/com/example/ool_mobile/ui/list/equipment/EquipmentListFragment.java
@@ -1,6 +1,13 @@
 package com.example.ool_mobile.ui.list.equipment;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.ool_mobile.R;
 
@@ -10,4 +17,15 @@ public class EquipmentListFragment extends Fragment {
         super(R.layout.fragment_list_equipment);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getView().findViewById(R.id.listEquipment_fab).setOnClickListener(v -> {
+
+            NavController controller = Navigation.findNavController(getView());
+
+            controller.navigate(R.id.action_navigation_equipments_to_addEquipmentActivity);
+        });
+    }
 }
