7a6453081ffc974b9c1445d6e0646d25bee6b5d7
figurantpp <renan.r.marcelino2@protonmail.com>
2021-08-29 11:59:18 -0300

    Added AddEmployeeActivity

--- app/src/main/java/com/example/ool_mobile/ui/list/employee_list/EmployeeListFragment.java
+++ app/src/main/java/com/example/ool_mobile/ui/list/employee_list/EmployeeListFragment.java
@@ -8,10 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.ool_mobile.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EmployeeListFragment extends Fragment {
+
 
     @Nullable
     @Override
@@ -21,5 +25,19 @@ public class EmployeeListFragment extends Fragment {
             @Nullable Bundle savedInstanceState
     ) {
         return inflater.inflate(R.layout.fragment_list_employee, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FloatingActionButton button = getView().findViewById(R.id.listEmployee_addButton);
+
+        button.setOnClickListener(v -> {
+
+            NavController controller = Navigation.findNavController(getView());
+
+            controller.navigate(R.id.action_navigation_employees_to_addEmployeeActivity);
+        });
     }
 }
