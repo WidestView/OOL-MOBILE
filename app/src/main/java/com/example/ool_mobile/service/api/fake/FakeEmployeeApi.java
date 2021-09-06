325380ac5df530fbdb2cb7677ace5349b4b732b4
figurantpp <renan.r.marcelino2@protonmail.com>
2021-09-06 06:40:07 -0300

    Added Firing fragment

---
+++ app/src/main/java/com/example/ool_mobile/service/api/fake/FakeEmployeeApi.java
@@ -0,0 +1,5 @@
+package com.example.ool_mobile.service.api.fake;
+
+public class FakeEmployeeApi {
+
+}
--- app/src/main/java/com/example/ool_mobile/ui/calendar/CalendarViewFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.ool_mobile.ui.calendar;
-
-import androidx.fragment.app.Fragment;
-
-public class CalendarViewFragment extends Fragment {
-}
