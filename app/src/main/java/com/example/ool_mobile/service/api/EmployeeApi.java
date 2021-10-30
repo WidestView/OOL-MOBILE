66a28ad12b9abd4837d0943e60545ea9e830fef1
figurantpp <renan.r.marcelino2@protonmail.com>
2021-10-30 15:52:28 -0300

    Implemented EmployeeApi listing

--- app/src/main/java/com/example/ool_mobile/service/api/EmployeeApi.java
+++ app/src/main/java/com/example/ool_mobile/service/api/EmployeeApi.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 
 import com.example.ool_mobile.model.Employee;
 
+import java.util.List;
+
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.Response;
 import retrofit2.http.GET;
@@ -14,4 +16,7 @@ public interface EmployeeApi {
     @GET("employee/info")
     Single<Response<Employee>> getCurrentEmployeeInfo();
 
+    @NonNull
+    @GET("employee")
+    Single<List<Employee>> listEmployees();
 }
