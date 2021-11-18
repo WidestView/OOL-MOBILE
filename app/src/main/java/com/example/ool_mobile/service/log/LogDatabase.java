bbafd72f68ad6ded410c088cb3e17ceef8daa2b0
figurantpp <renan.r.marcelino2@protonmail.com>
2021-11-18 08:47:06 -0300

    Added entry listing support to log database

--- app/src/main/java/com/example/ool_mobile/service/log/LogDatabase.java
+++ app/src/main/java/com/example/ool_mobile/service/log/LogDatabase.java
@@ -2,7 +2,14 @@ package com.example.ool_mobile.service.log;
 
 import androidx.annotation.NonNull;
 
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Single;
+
 public interface LogDatabase extends AutoCloseable {
 
     void addEntry(@NonNull LogEntry entry);
+
+    @NonNull
+    Single<List<LogEntry>> listEntries();
 }
--- app/src/main/java/com/example/ool_mobile/service/log/SQLiteLogDatabase.java
+++ app/src/main/java/com/example/ool_mobile/service/log/SQLiteLogDatabase.java
@@ -2,13 +2,19 @@ package com.example.ool_mobile.service.log;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
 
 public class SQLiteLogDatabase extends SQLiteOpenHelper implements LogDatabase {
 
@@ -64,6 +70,32 @@ public class SQLiteLogDatabase extends SQLiteOpenHelper implements LogDatabase {
 
         database.insert("Logs", null, values);
 
+    }
+
+    @NonNull
+    @Override
+    public Single<List<LogEntry>> listEntries() {
+
+        return Observable.<LogEntry>create(emitter -> {
+
+            try (Cursor cursor = database.rawQuery("select * from Logs", null)) {
+
+                while (cursor.moveToNext()) {
+
+                    emitter.onNext(
+                            ImmutableLogEntry.builder()
+                                    .priority(cursor.getString(0))
+                                    .date(new Date(cursor.getLong(1)))
+                                    .tag(cursor.getString(2))
+                                    .text(cursor.getString(3))
+                                    .exceptionMessage(cursor.getString(4))
+                                    .build()
+                    );
+
+                }
+            }
+
+        }).toList();
     }
 
 
